9c8f5551219629188268528da0d80d52fa9f2c2d
barisayyildiz <barisayyildiz.1799@gmail.com>
2023-06-08 21:40:46 +0300

    client.py dan gelen veriler doğrultusunda object detection yapılabiliyor

--- main.cpp
+++ main.cpp
@@ -9,6 +9,18 @@
 #include <QDebug>
 #include <opencv2/opencv.hpp>
 
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+#define PORT 8000  // Port number to listen on
+
 // Function to apply non-maximum suppression
 void applyNMS(std::vector<cv::Rect>& boxes, std::vector<float>& confidences, float nmsThreshold)
 {
@@ -138,18 +150,109 @@ int main(int argc, char *argv[])
     // Initialize the server communication and receive the frame
     // Replace this part with your server communication code from server.cpp
 
-    cv::VideoCapture cap(0);
-    if (!cap.isOpened()) {
-        qWarning() << "Failed to open the camera.";
-        return -1;
+
+
+
+    int server_socket, new_socket, valread;
+    struct sockaddr_in address{};
+    int addrlen = sizeof(address);
+
+    // Create server socket
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind the socket to the specified port
+    if (bind(server_socket, (struct sockaddr *) &address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_socket, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Accept incoming connection
+    if ((new_socket = accept(server_socket, (struct sockaddr *) &address, (socklen_t *) &addrlen)) < 0) {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    std::vector<uchar> buffer;
+    int message_size;
+
+    std::cout << "test..." << "\n";
+    int counter = 0;
+
+
     cv::Mat frame;
+
+
+
     QTimer timer;
     QObject::connect(&timer, &QTimer::timeout, [&]() {
-        cap >> frame;
+        //cap >> frame;
+
+        std::cout << "debug1\n";
+        if (read(new_socket, &message_size, sizeof(message_size)) == 0) {
+            perror("Client disconnected");
+            return 1;
+        }
+        std::cout << "debug2\n";
+
+        std::cout << "message size : " << message_size << "\n";
+
+        // Resize the buffer to fit the received frame
+        buffer.resize(message_size);
+
+        auto start = high_resolution_clock::now();
+        ssize_t bytes_read;
+        ssize_t total_bytes_read = 0;
+        while (total_bytes_read < message_size)
+        {
+            std::cout << "total_bytes_read : " << total_bytes_read << "\n";
+            ssize_t bytes_read = read(new_socket, buffer.data() + total_bytes_read, message_size - total_bytes_read);
+            std::cout << "bytes_read : " << bytes_read << "\n";
+            if (bytes_read == 0)
+            {
+                // Connection closed by the sender
+                break;
+            }
+            else if (bytes_read < 0)
+            {
+                // Error reading the byte data
+                std::cerr << "Failed to read the byte data." << std::endl;
+                break;
+            }
+
+            total_bytes_read += bytes_read;
+        }
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        cout << "Time taken by function: "
+            << duration.count() * 0.001 << " miliseconds" << endl;
+
+
+        std::cout << "bytes_read : " << bytes_read << "\n";
+        for(int i=0; i<10; i++){
+            std::cout << "byte number " << i << " " << int(buffer[i]) << "\n";
+        }
+
+        start = high_resolution_clock::now();
+
+        // Deserialize the frame
+        frame = cv::imdecode(buffer, cv::IMREAD_COLOR);
+
+
         if (frame.empty())
-            return;
+            return 1;
 
         // Update the frame in the GUI
         updateFrame(frame, frameLabel);
@@ -161,8 +264,6 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
-
-
 
 
 /*
